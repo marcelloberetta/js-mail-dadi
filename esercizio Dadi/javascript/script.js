e78f8f683e05d1106d1d364f5086e9e53d77fb04
marcelloberetta <marcelloberetta4@gmail.com>
2021-04-08 23:38:20 +0200

    Added style & Output modifies

--- esercizio Dadi/javascript/script.js
+++ esercizio Dadi/javascript/script.js
@@ -8,24 +8,24 @@ alert("Giochiamo a dadi. Premi OK per effettuare il tuo lancio");
 
 var dadoUtente = Math.floor(Math.random() * 6 ) + 1 ;
 
-document.getElementById("lancioUtente").innerHTML = "Lancio utente =  " + dadoUtente;
+document.getElementById("lancioUtente").innerHTML = "Lancio " + userName + " --> " + dadoUtente;
 
 
 var dadoCpu = Math.floor(Math.random() * 6 ) + 1 ;
 
-document.getElementById("lancioCpu").innerHTML = "Lancio CPU = " + dadoCpu;
+document.getElementById("lancioCpu").innerHTML = "Lancio CPU --> " + dadoCpu;
 
 
 if (dadoCpu > dadoUtente){
-   document.getElementById("risultato").innerHTML = "<strong> Hai perso </strong>"
+   document.getElementById("risultato").innerHTML = "<strong> Mi dispiace " + userName + " , hai perso </strong>"
 }
 
 else if (dadoCpu === dadoUtente){
-   document.getElementById("risultato").innerHTML = "<strong> Hai pareggiato </strong>"
+   document.getElementById("risultato").innerHTML = "<strong> Hai pareggiato contro il computer</strong>"
 }
 
 else{
-   document.getElementById("risultato").innerHTML = " <strong> Hai vinto </strong>"
+   document.getElementById("risultato").innerHTML = " <strong> Complimenti " + userName + " , hai vinto </strong>"
 }
 
 
