b629b660165ea3207b120293da29368a8bd95358
marcelloberetta <marcelloberetta4@gmail.com>
2021-04-08 16:48:59 +0200

    Initial commit "Esercizio Dadi"

---
+++ esercizio Dadi/javascript/script.js
@@ -0,0 +1,12 @@
+var userName = prompt("Ciao ! come ti chiami ?");
+
+// console.log(userName);
+
+alert("Benvenuto " + userName);
+
+// * INFORMIAMO L'UTENTE SUL GIOCO E CHIEDIAMO IL NUMERO
+var userNumber = prompt("Giochiamo a dadi. Scegli un numero da 1 a 12");
+
+// console.log(userNumber);
+
+
